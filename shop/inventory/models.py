3ea691a1b92b20fee83d76509b55453aaa467bd6
levanberadze <163711921+levanberadze@users.noreply.github.com>
2024-12-07 15:19:41 +0400

    Add files via upload

---
+++ shop/inventory/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.db.models import Avg, Sum, Count
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.IntegerField()
+    rating = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
