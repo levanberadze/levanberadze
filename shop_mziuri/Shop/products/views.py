8746fe13a55bcefbe24e535a9ae458ab98ded308
levanberadze <163711921+levanberadze@users.noreply.github.com>
2024-12-14 17:28:16 +0400

    Add files via upload

--- shop_mziuri/Shop/products/views.py
+++ shop_mziuri/Shop/products/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 
 def home(request):
-    products = Product.objects.all()
-    return render(request, 'home.html', {'products': products})
+    product_name = request.GET.get('product_name')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    category = request.GET.get('category')
+    in_stock = request.GET.get('in_stock')
+
+    filters = dict()
+
+
+    if product_name:
+        filters['name__icontains'] = product_name
+
+    if min_price:
+        filters['price__gt'] = min_price
+
+    if max_price:
+        filters['price__lt'] = max_price
+
+    if in_stock == 'on':
+        filters['stock_qty__gt'] = 0
+
+    if category:
+        filters['category'] = category
+
+    products = Product.objects.filter(**filters)
+
+    categories = Category.objects.all()
+
+    return render(request, 'home.html', {'products': products, 'categories': categories})
 
 
 def product_detail(request):
