efe077dbc2ea60fbc14faf0101be961d67df11a7
g0r81 <dagiojanadze@gmail.com>
2024-12-21 17:34:11 +0400

    users

--- shop_mziuri/Shop/Shop/settings.py
+++ shop_mziuri/Shop/Shop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'users',
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
